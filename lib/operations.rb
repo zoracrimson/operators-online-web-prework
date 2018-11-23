7e0df9e96404603311f549f8185d15f522eea2d1
Kat Maldon <katharinamaldon@gmail.com>
2018-11-23 16:56:16 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,22 @@
 def unsafe?(speed)
-
+  if speed > 60
+    return true 
+  end 
+  if speed < 40
+    return true
+  end
+  if speed < 60 && speed > 40
+  return false
+  end 
 end
 
 
 
+
 def not_safe?(speed)
-	
+  if speed > 60 
+    return true
+  end
 end
 	
 
